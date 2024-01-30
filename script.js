a6641c92ef64168d3690de46713dfa0dfae06b86
thearhansheth <arhan.sheth@gmail.com>
2024-01-30 11:59:45 -0500

    Added conditional statements to playRound() to simulate a round of the game

--- script.js
+++ script.js
@@ -4,6 +4,38 @@ function getComputerChoice() {
   return choice[selectedChoice];
 }
 
-for (let i = 0; i <= 4; i++) {
-  console.log(getComputerChoice());
+function playRound(userInputChoice, computerChoice) {
+  if (
+    userInputChoice.toLowerCase() != "rock" ||
+    userInputChoice.toLowerCase() != "paper" ||
+    userInputChoice.toLowerCase() != "scissors"
+  ) {
+    return "Invalid Choice!";
+  }
+  if (userInputChoice.toLowerCase() === computerChoice.toLowerCase()) {
+    return "No winner, try again!";
+  } else if (computerChoice.toLowerCase() === "rock") {
+    if (userInputChoice.toLowerCase() === "paper") {
+      return "You win! Paper beats Rock!";
+    } else {
+      return "You lose! Rock beats Scissors!";
+    }
+  } else if (computerChoice.toLowerCase() === "paper") {
+    if (userInputChoice.toLowerCase() === "scissors") {
+      return "You win! Scissors beats Paper!";
+    } else {
+      return "You lose! Paper beats Rock!";
+    }
+  } else if (computerChoice.toLowerCase() === "scissors") {
+    if (userInputChoice.toLowerCase() === "rock") {
+      return "You win! Rock beats Scissors!";
+    } else {
+      return "You lose! Scissors beats Paper";
+    }
+  }
 }
+
+userSelection = prompt("Select Rock, Paper or Scissors");
+computerSelection = getComputerChoice();
+result = playRound(userSelection, computerSelection);
+console.log(result);
