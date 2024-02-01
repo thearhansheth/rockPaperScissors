a8874066f5d8db098baf59ed1df716cc03e01919
thearhansheth <arhan.sheth@gmail.com>
2024-02-01 14:27:15 -0500

    Added game elements and score counter variables

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+//Game Elements
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(userInputChoice, computerChoice) {
+  console.log(userInputChoice.toLowerCase());
+  console.log(computerChoice);
+  if (
+    userInputChoice.toLowerCase() != "rock" &&
+    userInputChoice.toLowerCase() != "paper" &&
+    userInputChoice.toLowerCase() != "scissors"
+  ) {
+    return "Invalid Choice!";
+  }
+  if (userInputChoice.toLowerCase() === computerChoice.toLowerCase()) {
+    return "No winner, try again!";
+  } else if (computerChoice.toLowerCase() === "rock") {
+    if (userInputChoice.toLowerCase() === "paper") {
+      playerScore++;
+      return "You win! Paper beats Rock!";
+    } else {
+      computerScore++;
+      return "You lose! Rock beats Scissors!";
+    }
+  } else if (computerChoice.toLowerCase() === "paper") {
+    if (userInputChoice.toLowerCase() === "scissors") {
+      playerScore++;
+      return "You win! Scissors beats Paper!";
+    } else {
+      computerScore++;
+      return "You lose! Paper beats Rock!";
+    }
+  } else if (computerChoice.toLowerCase() === "scissors") {
+    if (userInputChoice.toLowerCase() === "rock") {
+      playerScore++;
+      return "You win! Rock beats Scissors!";
+    } else {
+      computerScore++;
+      return "You lose! Scissors beats Paper";
+    }
+  }
+}
+
 //UI Elements
 const rockBtn = document.getElementById("Rockbtn");
 const paperBtn = document.getElementById("Paperbtn");
@@ -21,43 +64,15 @@ if (scissorsBtn) {
 }
 
 //functions
+
+function buttonClicked(playerSelection) {
+  console.log(`Player selected ${playerSelection}`);
+  computerSelection = getComputerChoice();
+}
 function getComputerChoice() {
   let choice = ["rock", "paper", "scissors"];
   let selectedChoice = Math.floor(Math.random() * choice.length);
   return choice[selectedChoice];
-}
-
-function playRound(userInputChoice, computerChoice) {
-  console.log(userInputChoice.toLowerCase());
-  console.log(computerChoice);
-  if (
-    userInputChoice.toLowerCase() != "rock" &&
-    userInputChoice.toLowerCase() != "paper" &&
-    userInputChoice.toLowerCase() != "scissors"
-  ) {
-    return "Invalid Choice!";
-  }
-  if (userInputChoice.toLowerCase() === computerChoice.toLowerCase()) {
-    return "No winner, try again!";
-  } else if (computerChoice.toLowerCase() === "rock") {
-    if (userInputChoice.toLowerCase() === "paper") {
-      return "You win! Paper beats Rock!";
-    } else {
-      return "You lose! Rock beats Scissors!";
-    }
-  } else if (computerChoice.toLowerCase() === "paper") {
-    if (userInputChoice.toLowerCase() === "scissors") {
-      return "You win! Scissors beats Paper!";
-    } else {
-      return "You lose! Paper beats Rock!";
-    }
-  } else if (computerChoice.toLowerCase() === "scissors") {
-    if (userInputChoice.toLowerCase() === "rock") {
-      return "You win! Rock beats Scissors!";
-    } else {
-      return "You lose! Scissors beats Paper";
-    }
-  }
 }
 
 // function playGame() {
